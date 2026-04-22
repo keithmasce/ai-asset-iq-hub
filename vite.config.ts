474b7ea006bf61054aa0032ba291761612db9666
keithmasce <keith.mascarenhas@gmail.com>
2026-04-22 12:27:39 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  // We are passing this directly to the underlying Vite config
   vite: {
-    // This forces the Nitro server engine to build for Vercel
     nitro: {
-      preset: "vercel",
+      preset: "vercel", // Forces the creation of .vercel/output
     },
   },
 });
